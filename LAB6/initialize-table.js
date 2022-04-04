ddf48300bd58739effcb9c74559d21510f6bfe61
Hossein <hossein.keipour@gmail.com>
2022-04-04 22:19:13 +0200

    update

--- LAB6/initialize-table.js
+++ LAB6/initialize-table.js
@@ -1,10 +1,34 @@
 
 function initializeTable() {
-     // TODO
-    $("#createLink").click(createCountry);
-    let country = $("#newCountryText").val();
-    let capital = $("#newCapitalText").val();
+    // TODO
     
     
+    
+    function createCountry() {
+        let country = $('#newCountryText').val();
+        let capital = $('#newCapitalText').val();
+        addCountryCapital(country, capital, true);
+        $('#newCountryText').val('');
+        $('#newCapitalText').val('');
+        actionSetting();
+    }
+    function addCountryCapital(country, capital){
+        newRow = $("<tr>").append("<td>"+text(country)).append("<td>"+text(capital)).append("<td>"+text(<a href="#" id="down">[Down]</a>))
+    }
+
+    function moveUp(){
 
     }
+    function moveDown(){
+        
+    }
+    function del(){
+        
+    }
+    function moveRowUp(){
+        
+    }
+    function actionSetting(){
+
+    }
+}
