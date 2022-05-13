67f459506840feb32cb83d03fcbc4db9d918e102
Hossein <hossein.keipour@gmail.com>
2022-05-13 14:44:21 +0200

    LAB 1 Django

--- Django/Project_two/Project_two/urls.py
+++ Django/Project_two/Project_two/urls.py
@@ -13,9 +13,19 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+# from django.conf.urls import url
+# from django.contrib import admin
+
+# urlpatterns = [
+#     url(r'^admin/', admin.site.urls),
+# ]
+
 from django.contrib import admin
+from django.conf.urls import url
+from AppTwo import views
+
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+    url(r'^$', views.index, name = 'index'),
+    url('admin/', admin.site.urls),
 ]
