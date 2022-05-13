9dd81d82ee98d74e0f7c34f34c91f62eb23b7fb4
Hossein <hossein.keipour@gmail.com>
2022-05-13 12:21:48 +0200

    del

--- Django/first_project/helloworldapp/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-from django.shortcuts import HttpResponse
-
-# Create your views here.
-def home(request):
-    return HttpResponse("Hello world from Django")
