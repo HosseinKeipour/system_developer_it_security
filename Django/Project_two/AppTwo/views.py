84cc82dc7bef06d5d7d44b18e33f917f264f9a16
Hossein <hossein.keipour@gmail.com>
2022-05-16 12:12:31 +0200

    update

--- Django/Project_two/AppTwo/views.py
+++ Django/Project_two/AppTwo/views.py
@@ -2,8 +2,5 @@ from django.shortcuts import render
 
 # Create your views here.
 
-from django.shortcuts import HttpResponse
-
-# Create your views here.
 def index(request):
     return render(request, 'index.html')
